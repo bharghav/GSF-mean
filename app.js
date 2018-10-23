b6a7d472810ca9684597b4cc9cddeb149b2be416
bharghav-satsyil <bharghav.chinna@satsyil.com>
2018-10-23 15:20:24 +0530

    error message updated

--- app.js
+++ app.js
@@ -9,7 +9,20 @@ const usersRoutes = require('./api/routes/users');
 //Routes with handle server
 app.use('/users',usersRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status(404);
+    next(error);
+});
 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 // app.use((req, res, next) => {
 //     res.status(200).json({
 //         message : "itworks !"
